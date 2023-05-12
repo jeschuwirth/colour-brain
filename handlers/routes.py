22dd43073af05c412c2027f2a72c16a988cbd904
Juan Schuwirth M <jeschuwirth@uc.cl>
2023-05-11 21:04:20 -0400

    First commit

---
+++ handlers/routes.py
@@ -0,0 +1,12 @@
+#Import handlers
+
+from handlers.handle_message import handle_message
+
+#Add routes and handlers to the dict {"route_key": handler_function}
+route_dic = {
+    "sendmessage", handle_message
+}
+
+def routes(route_key, table, event, connection_id, apig_management_client):
+    action = route_dic[route_key]
+    action(table, event, connection_id, apig_management_client)
